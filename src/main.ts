7e06338eb74e28365d014b6af7e4814385e87dea
Thomas Wessel <turpleturtle12@gmail.com>
2024-10-19 11:17:24 -0700

    Step 2 complete
    Add canvas and clear button

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,51 @@
 import "./style.css";
 
 const APP_NAME = "Drawing App";
+const CANVAS_SIZE = 256;
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
+////**** Page Content ****////
 document.title = APP_NAME;
 
 const header = document.createElement("h1");
 header.innerHTML = APP_NAME;
-app.append(header);
 
 const canvas = document.createElement("canvas");
-canvas.width = canvas.height = 256;
-app.appendChild(canvas);
+canvas.width = canvas.height = CANVAS_SIZE;
+
+const clearButton = document.createElement("button");
+clearButton.innerHTML = "Clear Canvas";
+clearButton.addEventListener("click", () => {
+    const ctx = canvas.getContext("2d")!;
+    ctx.clearRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+});
+
+app.append(header, canvas, clearButton);
+
+////**** Drawing with Mouse ****////
+let isDrawing = false;
+let cursorX = 0;
+let cursorY = 0;
+
+const canvasRenderer = canvas.getContext("2d")!;
+
+canvas.addEventListener("mousedown", (e) => {
+    cursorX = e.offsetX;
+    cursorY = e.offsetY;
+    isDrawing = true;
+});
+
+canvas.addEventListener("mousemove", (e) => {
+    if (!isDrawing) return;
+
+    canvasRenderer.beginPath();
+    canvasRenderer.moveTo(cursorX, cursorY);
+    cursorX = e.offsetX;
+    cursorY = e.offsetY;
+    canvasRenderer.lineTo(cursorX, cursorY);
+    canvasRenderer.stroke();
+});
+
+canvas.addEventListener("mouseup", () => {
+    isDrawing = false;
+});
